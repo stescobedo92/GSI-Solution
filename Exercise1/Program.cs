e5d1c11e83320f58faf327ac51a740d713dccdea
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-03-29 10:09:06 -0600

    first commit

---
+++ Exercise1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise1
+{
+    class Program
+    {
+/*
+        that, given an array A of N integers, returns the smallest positive integer (greater than 0) that
+        does not occur in A.
+         
+         For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+            Given A = [1, 2, 3], the function should return 4.
+            Given A = [−1, −3], the function should return 1.
+        
+        Write an efficient algorithm for the following assumptions:
+        ● N is an integer within the range [1..100,000];
+        ● each element of array A is an integer within the range [−1,000,000..1,000,000]
+        */
+        public static int Smallest(int[] numbers)
+        {
+            HashSet<int> set = new HashSet<int>();
+            int index = 1;
+
+            foreach(var number in numbers)
+            {
+                if(number > 0)
+                {
+                    set.Add(number);
+                }
+            }
+
+            while(set.Contains(index))
+            {
+                index++;
+            }
+
+            return index;
+        }
+
+        static void Main(string[] args)
+        {
+            var numbers = new int[]{1,3,6,4,1,2};
+            Console.WriteLine("{0}",Smallest(numbers));
+        }
+    }
+}
---
+++ Exercise2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        /*
+        Find the bug(s) and modify one line on the incorrect implementation of the function solution that
+        is supposed to return the smallest the element on the given non-empty list "numbers" which
+        contains at most 1000 integers within the range [-1000..1000]
+        Notice that the example test case "var numbers = <int>[-1, 1, -2, 2];" the attached code is
+        already returning the correct answer (-2)        
+        */
+        public static int FindSmallest(int[] numbers){
+            int small = numbers[0];
+            for(int i = 1; i < numbers.Length;i++){
+                if(numbers[i] < small ){
+                    small = numbers[i];
+                }
+            }
+
+            return small;
+        }
+
+        static void Main(string[] args)
+        {
+            var numbers = new int[]{-1,1,-2,2};
+            Console.WriteLine("{0}",FindSmallest(numbers));
+        }
+    }
+}
