de57590acbe96f1e0ff356f1adf3d53c42b1620e
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-03-29 10:30:22 -0600

    Added Project entity

---
+++ Exercise3/BugTracking/BugTracking.API/Domain/Models/Project.cs
@@ -0,0 +1,9 @@
+namespace BugTracking.API.Domain.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get;set; }
+    }
+}
